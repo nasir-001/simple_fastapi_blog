bfdde290ec075c1f073e1748b54945d48f957e9e
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-20 00:05:44 +0100

    query parameter

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': 'blog list'}
+@app.get('/blogs')
+def index(published: bool = True, limit: int = 10, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from database'}
+    else:
+        return {'data': f'{limit} blogs from database'}
 
 
 @app.get('/blog/unpublished')
@@ -19,5 +23,5 @@ def singleBlog(id: int):
 
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit=10):
     return {'data': {'1', '2'}}
