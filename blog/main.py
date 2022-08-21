ee2e7235f228ef15f8db360f526ecc3253c9b988
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-21 10:58:30 +0100

    connect database

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from . import schemas
+from . import schemas, models
+from .database import engine
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
 
 @app.post('/blog')
 def  create(blog: schemas.Blog):
