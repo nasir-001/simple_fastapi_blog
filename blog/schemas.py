c0327f5fc4170b5c720c2d901b84e2dfdd5d2b82
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-24 18:19:55 +0100

    add blog and user relationship

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,9 +1,13 @@
 from pydantic import BaseModel
+from typing import List
 
 
 class Blog(BaseModel):
     title: str
     body: str
+
+    class Config:
+        orm_mode = True
 
 
 
@@ -17,6 +21,7 @@ class User(BaseModel):
 class ShowUser(BaseModel):
     name: str
     email: str
+    blogs: List[Blog] = []
 
     class Config:
         orm_mode = True
